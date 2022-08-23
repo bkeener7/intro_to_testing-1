9119a516c2308de35d3404e66b0603451b09c814
Bryan Keener <bkeener7@gmail.com>
2022-08-23 16:04:45 -0600

    add student files

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,13 +1,30 @@
 # Add the 2 requires you will need here
+require 'rspec'
+require './lib/student'
 
-
-describe Student do
+RSpec.describe Student do
   describe '#initialize' do
     it 'is an instance of student' do
       # write the code to initialize a new student object
+      student = Student.new('Penelope')
+      expect(student).to be_instance_of Student
+      
     end
     # test it has a name
+    it 'has a name' do
+      student = Student.new('Penelope')
+      expect(student.name).to eq ('Penelope')
+    end
     # test it has cookies
+    it 'has cookies' do
+      student = Student.new('Penelope')
+      expect(student.cookies).to eq([])
+    end
     # test it can add cookies
+    it 'can add cookies' do
+      student = Student.new('Penelope')
+      student.add_cookie('Chocolate Chip')
+      expect(student.cookies).to eq(['Chocolate Chip'])
+    end
   end
 end
